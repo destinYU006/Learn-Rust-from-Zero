5d1a80432c923bacd2694237102a7681d3df9b35
destinYU006 <dengtaoyu15221450952@outlook.com>
2025-05-24 09:08:58 +0800

    Update clippy2.rs
    增加了内存解读和编译解读

--- clippy2.rs
+++ clippy2.rs
@@ -6,9 +6,10 @@
 // I AM NOT DONE
 
 fn main() {
-    let mut res = 42;
-    let option = Some(12);
-    for x in option.iter() {
+    let mut res = 42;       //res会被分配 4 个字节的空间（这是因为 i32 类型占 4 字节），并且初始值为 42。
+    let option = Some(12);  //option则会分配 8 个字节（因为 Option<i32>属于枚举类型，包含一个 i32 值和一个用于判别是否为 Some 的标记位，共占 8 字节），它会被初始化为 Some (12)。
+    for x in option.iter() { //迭代器存储：在栈上，option.iter()会生成一个迭代器，不过这个迭代器是零大小的，也就是说它不会占用实际的内存空间。
+//循环变量x是对 option 内部值的引用，它在栈上会占用 8 个字节（这是 64 位系统上引用的大小）。
         res += x;
     }
     println!("{}", res);
@@ -21,3 +22,8 @@ fn main() {
 //  if let Some(x) = option {
 //         res += x;
 //     }
+// 编译器处理过程
+// 类型检查：编译器会确认option的类型是Option<i32>，而option.iter()返回的是一个迭代器，该迭代器会产生&i32类型的值。
+// 循环展开：由于option是Some(12)，所以循环体只会执行一次。要是option的值为None，循环体就不会执行。
+// 借用检查：编译器会保证在option被借用期间，其值不会被修改。
+// 不可变引用：x是对option内部值的不可变引用，所以无法通过x来修改option的值。
